0c16638694b8e0f3a1353a16b36ce3c4585fb484
Tyler Schrock <tschrock123@gmail.com>
2018-05-17 10:16:35 -0400

    Clean up index.ts

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 'use strict';
 
-//import * as yargs from 'yargs';
+// import * as yargs from 'yargs';
 import yargs = require('yargs')
 
 const argv = yargs
-    
+
     // Config options
     .options({
         'server': {
@@ -35,82 +35,27 @@ const argv = yargs
             conflicts: ['username', 'password']
         }
     })
-    
+
     // Source the config.json file
     .config({
-        extends: './config.json',
-        logLevel: 'verbose'
+        extends: './config.json'
     })
-    
+
     // Source PM_* environment variables
     .env('PM')
-    
+
     // Use commands in ./commands
     .commandDir('commands')
-    
+
     // Force using a command
     .demandCommand()
 
     // Recommend spelling fixes
     .recommendCommands()
-    
+
     // Throw on invalid options/commands
-//    .strict()
+    .strict()
 
     .help()
     .version()
     .argv
-
-console.dir(argv);
-
-
-
-/*
-import { MacServerAuth } from './lib/MacServerAuth';
-import { ProfileManager } from './lib/ProfileManager';
-import { getConfig } from './lib/Config';
-
-const config = getConfig(argv);
-
-
-
-
-
-
-
-
-
-
-async function dostuff() {
-
-    // First, get an authorized session from the macserver itself
-    const macServerAuth = new MacServerAuth(config.server);
-    const sessionGuid = await macServerAuth.getAuthorizedSessionGuid(config.username, config.password);
-
-    // Then, pass that on to get an authorized session from Profile Manager
-    const profileManager = new ProfileManager(`${config.server}/devicemanagement`);
-    await profileManager.authorize(sessionGuid);
-
-    // Then, do magic things :D
-
-    // Lock a device
-    await profileManager.doMagic({"library_item_task":{"start_task":[[{"target_class":"Device","target_id":28,"params":{},"task_type":"DeviceLock"},"something"]]}});
-
-    // Locks all devices in a group
-    // await profileManager.doMagic({"library_item_task":{"start_task":[[{"target_class":"DeviceGroup","target_id":23,"params":{},"task_type":"DeviceLock"},"something"]]}});
-
-    // Restarts a device
-    // await profileManager.doMagic({"library_item_task":{"start_task":[[{"target_class":"Device","target_id":28,"params":{},"task_type":"RestartDevice"},"something"]]}});
-
-    // Restarts all devices in a group
-    // await profileManager.doMagic({"library_item_task":{"start_task":[[{"target_class":"DeviceGroup","target_id":23,"params":{},"task_type":"RestartDevice"},"something"]]}});
-
-    // Note: These are just adding tasks to be executed, there's no way to know if it succeeded or not without making another query
-}
-
-dostuff().then(
-    success => console.dir(success),
-    fail => console.log(fail)
-);
-
-*/
